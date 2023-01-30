eeeaa6e9ac166c868a7d39d31e8c2296a1c01342
yyokoo <revaldan1357@gmail.com>
2023-01-30 10:57:01 +0900

    バリデーション記述

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,8 @@ class Prototype < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concepet, presence: true
+  validates :image, presence: true
 end
