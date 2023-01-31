3b06521c52bf1c7c24203812879c24f8c8c8f950
yyokoo <revaldan1357@gmail.com>
2023-01-31 09:57:04 +0900

    編集機能の追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,8 +2,5 @@ class Prototype < ApplicationRecord
   belongs_to :user
 
   has_one_attached :image
-  validates :title, presence: true
-  validates :catch_copy, presence: true
-  validates :concepet, presence: true
-  validates :image, presence: true
+  validates :title, :catch_copy, :concept, :image, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'prototypes/index'
   root to: "prototypes#index"
+  resources :prototypes, only: [:new, :create, :show, :edit, :update]
+  
 end
